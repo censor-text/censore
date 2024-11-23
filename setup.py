70cd2f34228e841f9788d47fa8f4aa9240488516
Okinea Dev <okineadev@users.noreply.github.com>
2024-11-23 17:39:44 +0200

    chore: ⏭️ bump version to 0.2.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="censore",
     # noqa
-    version="0.2.0",
+    version="0.2.1",
     description="A package for censoring profanity in text",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
